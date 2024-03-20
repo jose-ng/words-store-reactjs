18e358976cff6118feb7ab590e6dbc2db6e75700
jose-ng <jose.ng@globant.com>
2024-03-20 15:20:20 -0600

    update look json

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Logo.module.scss";
 
 function Logo() {
-  return <h1 className={styles.Title}>Words translation</h1>;
+  return <h1 className={styles.Title}>English Words</h1>;
 }
 
 export default Logo;
