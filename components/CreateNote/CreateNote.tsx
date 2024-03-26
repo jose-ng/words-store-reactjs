b3072ebc5f29ab4f13a46f0c38c6380cc7f91381
jose-ng <jose.ng@globant.com>
2024-03-26 10:00:21 -0600

    add styles to inputs

--- components/CreateNote/CreateNote.tsx
+++ components/CreateNote/CreateNote.tsx
@@ -2,6 +2,7 @@ function CreateNote({ onSubmit, onChangeValue, form, sending }: any) {
   return (
     <form>
       <input
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         type="text"
         name="title"
         placeholder="Title"
@@ -10,6 +11,7 @@ function CreateNote({ onSubmit, onChangeValue, form, sending }: any) {
       />
       <br />
       <textarea
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         rows={4}
         name="text"
         placeholder="Text"
@@ -18,6 +20,7 @@ function CreateNote({ onSubmit, onChangeValue, form, sending }: any) {
       />
       <br />
       <input
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         type="text"
         name="code"
         placeholder="code"
@@ -25,7 +28,7 @@ function CreateNote({ onSubmit, onChangeValue, form, sending }: any) {
         onChange={onChangeValue}
       />
       <br />
-      <button type="submit" disabled={sending} onClick={onSubmit}>
+      <button type="submit" disabled={sending} onClick={onSubmit} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Send
       </button>
     </form>
--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -21,6 +21,7 @@ function Search({ onSearch }: any) {
     <section className={styles.SearchContainer}>
       <div>
         <input
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           autoComplete="Off"
           name="search"
           placeholder="Search a word"
