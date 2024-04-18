ccd78ee8f2cb617d9a40e9bf92c5610dc842cb7a
jose-ng <jose.ng@globant.com>
2024-04-18 15:58:56 -0600

    add workout component

---
+++ hooks/useWorkOut.tsx
@@ -0,0 +1,52 @@
+"use client"
+import URL_API from "../utils/env";
+import { useEffect, useState } from "react";
+
+function useWorkOut() {
+    const [openModalWorkOut, setOpenModalWorkOut] = useState(false);
+    const [listWords, setListWords] = useState([]);
+    const [skip, setSkip] = useState(0);
+    const [limitResult, setLimitResult] = useState(10);
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        if (listWords.length === 0) {
+            setLoading(true);
+            getRandomItem();
+        }
+    }, [listWords]);
+
+    const getRandomItem = async () => {
+        try {
+
+            const skipRandom = Math.floor(Math.random() * (skip || 30));
+            let res: any = await fetch(URL_API + '/word?q=&skip=' + skipRandom + '&limit=' + limitResult, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+            res = await res.json();
+            const tWords = res.totalWords;
+            setSkip(tWords / 10);
+            const words = res.words.map((word: any) => {
+                return {
+                    _id: word._id,
+                    text_en: word.text_en,
+                    text_es: word.text_es
+                };
+            });
+
+            setListWords(words.reverse());
+            setLoading(false);
+        } catch (err: any) {
+            setError(err);
+            setLoading(false);
+        }
+    };
+
+    return { openModalWorkOut, setOpenModalWorkOut, listWords, setListWords, error, loading };
+
+}
+
+export { useWorkOut };
