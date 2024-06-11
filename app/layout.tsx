a2054d12f794f587c590280e7c962b1a43d380b3
jose-ng <jose.ng@globant.com>
2024-06-11 11:03:09 -0600

    rename redux provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.scss";
 import dynamic from "next/dynamic";
 import AuthProvider from "@/providers/AuthProvider";
 import Guard from "@/guards/Guard";
-const ReduxProvider = dynamic(() => import("../utils/redux/redux-provider"), {
+const ReduxProvider = dynamic(() => import("../providers/ReduxProvider"), {
   ssr: false
 });
 
