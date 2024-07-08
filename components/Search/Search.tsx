2022b630f2876a562bc32cf5d91b7972c00b601c
jose-ng <jose.ng@globant.com>
2024-07-08 13:18:00 -0600

    update search styles

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 import React, { useState } from "react";
-import styles from "./Search.module.scss";
+// import styles from "./Search.module.scss";
 
 function Search({ onSearch }: any) {
   const [searchForm, setSearchForm] = useState({ search: "" });
@@ -18,31 +18,30 @@ function Search({ onSearch }: any) {
   };
 
   return (
-    <section className={styles.SearchContainer}>
-      <div>
-        <input
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          autoComplete="Off"
-          name="search"
-          placeholder="Search a word"
-          value={searchForm.search}
-          onChange={handlerSearch}
-        ></input>
-        {searchForm.search && (
-          <button
-            type="button"
-            onClick={() => {
-              setSearchForm({
-                ...searchForm,
-                search: "",
-              });
-              onSearch("", true);
-            }}
-          >
-            x
-          </button>
-        )}
-      </div>
+    <section className="w-full relative inline-block">
+      <input
+        className="pr-10 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        autoComplete="Off"
+        name="search"
+        placeholder="Search a word..."
+        value={searchForm.search}
+        onChange={handlerSearch}
+      ></input>
+      {searchForm.search && (
+        <button
+          type="button"
+          className="button button-clear absolute right-2 top-2"
+          onClick={() => {
+            setSearchForm({
+              ...searchForm,
+              search: "",
+            });
+            onSearch("", true);
+          }}
+        >
+          <span className="material-icons">close</span>
+        </button>
+      )}
     </section>
   );
 }
