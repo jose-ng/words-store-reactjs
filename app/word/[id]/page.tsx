11b7215f62c356c4ce279c68a6517b7c1499f1ec
jose-ng <jose.ng@globant.com>
2024-08-20 14:07:46 -0600

    fix stye card

--- app/word/[id]/page.tsx
+++ app/word/[id]/page.tsx
@@ -1,9 +1,36 @@
 "use client";
+import { Card } from "@/components/Card/Card";
+import { useSearch } from "@/hooks/useSearch";
 import { Layout } from "@components/Layout/Layout";
-
-
+import { usePathname } from 'next/navigation';
+import { useEffect } from "react";
+// TODO: Implement this word detail component
 function Dashbard() {
-  return <Layout>id</Layout>;
+  const {
+    listWords,
+    setListWords,
+    showNotes,
+    setShowNotes,
+    nextResults,
+    setNextResults,
+    totalRecords,
+    totalShowRecords,
+    query,
+    loading,
+    handlerSearch,
+  } = useSearch();
+  const pathname = usePathname();
+  
+  useEffect(() => {
+    const paths = pathname.split('/');
+    
+  }, [pathname]);
+
+  return (
+    <Layout>
+      {/* <Card /> */}
+    </Layout>
+  );
 }
 
 export default Dashbard;
--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 function Card({ item, isLoggedIn, list }: any) {
@@ -5,13 +6,17 @@ function Card({ item, isLoggedIn, list }: any) {
   return (
     <div className="flex px-3 py-1 text-base text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white mb-1 items-center flex-wrap">
       {isLoggedIn && <span className="material-icons">drag_handle</span>}
-      <a href="#" className="pl-2 flex flex-wrap flex-col flex-1">
+      <Link
+        href={`/word/${item.id}`}
+        scroll={true}
+        className="pl-2 flex flex-wrap flex-col flex-1"
+      >
         <span className="flex items-center flex-wrap">
           <span>{text_en}</span>: <span>{text_es}</span>
         </span>
         {list && list.lenght && (
           <ul className="text-end">
-            <li className="inline-flex  px-2 py-0.5 ms-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">
+            <li className="inline-flex py-0.5 ms-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">
               Popular
             </li>
             <li className="inline-flex  px-2 py-0.5 ms-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">
@@ -19,7 +24,7 @@ function Card({ item, isLoggedIn, list }: any) {
             </li>
           </ul>
         )}
-      </a>
+      </Link>
     </div>
   );
 }
